1220e2259895cf364ab6636782c3296e840163f2
Alexander Henting <a.henting@posteo.de>
2023-09-02 20:12:49 +0200

    added supabase and connected it, frist data fetching in Products Component

--- src/roots/Products.tsx
+++ src/roots/Products.tsx
@@ -1,4 +1,40 @@
 import React from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+);
+
+type Product = {
+  id: string;
+  name: string;
+  stock: number;
+  brand: string;
+  created_at: string;
+  ean: number;
+};
+
 export default function Products(): React.ReactElement {
-  return <div>Product Page</div>;
+  const [products, setProducts] = React.useState<Product[]>([]);
+
+  React.useEffect(() => {
+    getProducts();
+  }, []);
+
+  async function getProducts() {
+    const { data } = await supabase.from("products").select();
+    if (data) {
+      setProducts(data);
+      console.log(data);
+    }
+  }
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.name}>{product.name} - {product.stock}</li>
+      ))}
+    </ul>
+  );
 }
